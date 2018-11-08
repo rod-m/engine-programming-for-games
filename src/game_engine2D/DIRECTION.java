c41d6e6287887df6b625e8667799dba57132a373
rod_martin10 <r.martin1@salford.ac.uk>
2018-11-08 15:12:29 +0000

    sprint 8 screens

---
+++ src/game_engine2D/DIRECTION.java
@@ -0,0 +1,5 @@
+package game_engine2D;
+
+public enum DIRECTION {
+	VERTICAL, HORIZONTAL
+}
--- src/game_engine2D/GUI/MenuMaker.java
+++ src/game_engine2D/GUI/MenuMaker.java
@@ -2,9 +2,12 @@ package game_engine2D.GUI;
 
 import java.util.ArrayList;
 
+import game_engine2D.ALIGNMENT;
+import game_engine2D.DIRECTION;
 import game_engine2D.GameObject;
 import game_engine2D.GameScreen;
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class MenuMaker extends GameObject {
 	/* Display a list of menu options
@@ -12,10 +15,12 @@ public class MenuMaker extends GameObject {
 	private ArrayList<String> menu_options = new ArrayList<String>();
 	// private int selected = 0;
 	private int w = 160,h = 30, padding = 15;
+
+	public DIRECTION direction = DIRECTION.HORIZONTAL;
+	public ALIGNMENT alignment = ALIGNMENT.BOTTOM_LEFT;
 	public MenuMaker(PApplet p) {
 		super(p);
-		//this.menu_options.add("Start");
-		//this.menu_options.add("Level Edit");
+		
 	}
 
 	
@@ -26,18 +31,19 @@ public class MenuMaker extends GameObject {
 		}
 		
 	}
+	
 	@Override
 	public void start() {
-		this.transform.position.x = parent.width /2 - w/2;
-		this.transform.position.y = parent.height /2 - h * this.menu_options.size();
-
+		if(this.alignment == ALIGNMENT.CENTRED) {
+			this.transform.position.x = parent.width /2 - w/2;
+			this.transform.position.y = parent.height /2 - h * this.menu_options.size();
+		}else {
+			this.transform.position.x = this.padding;
+			this.transform.position.y = this.padding;
+		}
+		
 	}
-
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 	@Override
 	public void render() {
@@ -47,12 +53,31 @@ public class MenuMaker extends GameObject {
 			//parent.rectMode(PApplet.CENTER);
 			parent.fill(0);
 			parent.stroke(255);
-			parent.rect(-this.padding, i * h, w, h);
+			if(this.direction == DIRECTION.VERTICAL) {
+				parent.rect(0, i * h, w, h);
+			}else {
+				parent.rect(i*w, 0, w, h);
+			}
+			
 			parent.fill(255);
 			parent.noStroke();
-			parent.text((i+1) + ": "+this.menu_options.get(i), 0,h/2 + 5+ i * h);
+			PVector textPos;
+			if(this.direction == DIRECTION.VERTICAL) {
+				textPos = new PVector(this.padding,h/2 + 5+ i * h);
+			}else {
+				textPos = new PVector(i * w +this.padding,h*0.7f);
+			}
+			parent.text((i+1) + ": "+this.menu_options.get(i), textPos.x,textPos.y);
+
 		}
 		parent.popMatrix();
+	}
+
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
